be84acb280541ad04df434df15385d493f82c549
DM524oigjroigre <131196777+DM524oigjroigre@users.noreply.github.com>
2024-09-28 13:22:39 -0700

    changed UI of BST quizzes

--- frontend/src/pages/DSA_quizzes.js
+++ frontend/src/pages/DSA_quizzes.js
@@ -37,7 +37,7 @@ const UserDashboard = () => {
 <h1>Additional Quizzes</h1>
 
 <Link to="/Linked_List_Quiz" style={{backgroundColor: '#6a0dad', color: 'white', padding: '15px 32px', margin: '10px', cursor: 'pointer', borderRadius: '5px', display: 'inline-block', textDecoration: 'none', fontSize: '16px', boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.2)', borderColor: 'black', border: '0px solid'}}>Linked List</Link>
-<button style={{backgroundColor: '#6a0dad', color: 'white', padding: '15px 32px', margin: '10px', cursor: 'pointer', borderRadius: '5px', display: 'inline-block', textDecoration: 'none', fontSize: '16px'}}>BST</button>
+<Link to="/BST_quiz" style={{backgroundColor: '#6a0dad', color: 'white', padding: '15px 32px', margin: '10px', cursor: 'pointer', borderRadius: '5px', display: 'inline-block', textDecoration: 'none', fontSize: '16px'}}>BST</Link>
 <button style={{backgroundColor: '#6a0dad', color: 'white', padding: '15px 32px', margin: '10px', cursor: 'pointer', borderRadius: '5px', display: 'inline-block', textDecoration: 'none', fontSize: '16px'}}>Open Hashing</button>
 <button style={{backgroundColor: '#6a0dad', color: 'white', padding: '15px 32px', margin: '10px', cursor: 'pointer', borderRadius: '5px', display: 'inline-block', textDecoration: 'none', fontSize: '16px'}}>Closed Hashmap</button>
 <button style={{backgroundColor: '#6a0dad', color: 'white', padding: '15px 32px', margin: '10px', cursor: 'pointer', borderRadius: '5px', display: 'inline-block', textDecoration: 'none', fontSize: '16px'}}>Selection Sort</button>
